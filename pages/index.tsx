e2b39a0649534e1a230a0fd05af8439969504a16
Elena Ivanova <31884088+elenaplehanova@users.noreply.github.com>
2022-11-01 19:14:57 +0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Sorting game</title>
+        <meta name="description" content="Sorting game for numbers from larger to smaller and vice versa. Created as test project for 'Sirius Future' company." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+        <h1 >
+          Sorting game
+        </h1>
+
+      </main>
+    </div>
+  )
+}
